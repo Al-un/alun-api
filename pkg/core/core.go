11f371d348a1b0e8b28c5374c3f8083770f0f76b
Al-un <alun.sng@gmail.com>
2019-12-20 18:58:59 +0900

    Adding package documentation

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -1,3 +1,10 @@
+// Package core is the essential package required by all other packages. While
+// all packages are, as much as possible, independent from each other, all of
+// them depend on the core package.
+//
+// Loggers are not in the core package only for "Separation of concerns" +
+// "Code like a library". Exposing the logging library should not bring all
+// the API related code with it.
 package core
 
 import "github.com/Al-un/alun-api/pkg/logger"
