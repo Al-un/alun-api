34b38271414dbffc5b7f11bef2943d479598af31
Al-un <alun.sng@gmail.com>
2019-12-27 12:02:46 +0100

    Adding routes display for debugging

--- pkg/core/api.go
+++ pkg/core/api.go
@@ -16,5 +16,15 @@ func SetupRouter(apis ...*API) *mux.Router {
 		(*api).LoadInRouter(router)
 	}
 
+	// r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+	// 	m, err := route.GetMethods()
+	// 	t, err := route.GetPathTemplate()
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	fmt.Println(m, " : ", t)
+	// 	return nil
+	// })
+
 	return router
 }
