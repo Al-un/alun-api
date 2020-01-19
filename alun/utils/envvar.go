33bf92ddac5eaa44ecab2d61cd3a33fa38f28d40
Al-un <alun.sng@gmail.com>
2020-01-20 00:01:14 +0900

    Adding client / server env var config

--- alun/utils/envvar.go
+++ alun/utils/envvar.go
@@ -2,11 +2,15 @@ package utils
 
 // List all environment variable names here to have a centralized list
 const (
-	// EnvVarMongoDbURI is the database URI access
-	EnvVarMongoDbURI    = "MONGODB_URI"
+	// === DB
+	EnvVarMongoDbURI = "MONGODB_URI"
+	// === Email
 	EnvVarEmailUsername = "ALUN_EMAIL_USERNAME"
 	EnvVarEmailPassword = "ALUN_EMAIL_PASSWORD"
 	EnvVarEmailHost     = "ALUN_EMAIL_HOST"
 	EnvVarEmailPort     = "ALUN_EMAIL_PORT"
 	EnvVarEmailSender   = "ALUN_EMAIL_SENDER"
+	// === Client / Server
+	EnvVarServerPort   = "ALUN_SERVER_PORT"
+	EnvVarClientDomain = "ALUN_CLIENT_DOMAIN"
 )
--- cmd/alun-api/main.go
+++ cmd/alun-api/main.go
@@ -4,22 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/Al-un/alun-api/alun/core"
 	"github.com/Al-un/alun-api/alun/memo"
+	"github.com/Al-un/alun-api/alun/utils"
 	"github.com/Al-un/alun-api/pkg/logger"
+	"github.com/joho/godotenv"
 )
 
-var serverPort = 8000
+const (
+	defaultPort = 8000
+)
+
+var serverPort int
 
 func main() {
+	rootLogger := logger.NewConsoleLogger(logger.LogLevelInfo)
+
+	// Env var loading
+	err := godotenv.Load()
+	if err != nil {
+		rootLogger.Fatal(1, "Error when load .env:\n%v", err)
+	}
+
+	// Server config
+	serverPort, err = strconv.Atoi(os.Getenv(utils.EnvVarServerPort))
+	if err != nil {
+		rootLogger.Info("Using default port %d", defaultPort)
+		serverPort = defaultPort
+	}
+
 	r := core.SetupRouter(
 		core.AuthAPI,
 		memo.MemoAPI,
 	)
 
-	rootLogger := logger.NewConsoleLogger(logger.LogLevelInfo)
-
+	// Go!
 	rootLogger.Info("[Server] Starting server on port %d...", serverPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", serverPort), r))
 }
