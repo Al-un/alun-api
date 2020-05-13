118f40787b84c21dde5e39d8deb3f6fdaf8de425
Al-un <alun.sng@gmail.com>
2020-05-14 01:06:57 +0900

    test: initialise apiTester instead of a mux.Router

--- alun/user/user_test.go
+++ alun/user/user_test.go
@@ -5,15 +5,14 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Al-un/alun-api/alun/core"
 	"github.com/Al-un/alun-api/alun/utils"
 	"github.com/Al-un/alun-api/pkg/logger"
-	"github.com/gorilla/mux"
+	"github.com/Al-un/alun-api/pkg/test"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 var (
-	testRouter        *mux.Router
+	apiTester         *test.APITester
 	testAuthToken     string
 	userNewEmail      string
 	userNewPassword   string
@@ -36,10 +35,7 @@ func setup() {
 	alunEmail = utils.GetDummyEmail()
 
 	// Setup router
-	testRouter = core.SetupRouter(
-		core.APIMicroservice,
-		UserAPI,
-	)
+	apiTester = test.NewAPITester(UserAPI)
 
 	// New user
 	userNewEmail = "test@test.com"
