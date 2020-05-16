a5b35edb8d698f17e255ad721a6e41e3f0117b42
Al-un <alun.sng@gmail.com>
2020-05-16 19:54:04 +0900

    test: clean up setup method and fix initial values

--- alun/user/user_test.go
+++ alun/user/user_test.go
@@ -7,10 +7,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Al-un/alun-api/alun/testutils"
 	"github.com/Al-un/alun-api/alun/utils"
 	"github.com/Al-un/alun-api/pkg/logger"
-	"github.com/Al-un/alun-api/pkg/test"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 const (
@@ -24,10 +25,11 @@ const (
 )
 
 var (
-	apiTester *test.APITester
+	apiTester *testutils.APITester
 	userAdmin = User{
 		BaseUser: BaseUser{Email: userAdminEmail},
 		IsAdmin:  true,
+		Username: userAdminUsername,
 		PwdResetToken: pwdResetToken{
 			Token:     "adminToken",
 			CreatedAt: time.Now(),
@@ -36,7 +38,8 @@ var (
 	}
 	userBasic = User{
 		BaseUser: BaseUser{Email: userBasicEmail},
-		IsAdmin:  true,
+		IsAdmin:  false,
+		Username: userBasicUsername,
 		PwdResetToken: pwdResetToken{
 			Token:     "basicToken",
 			CreatedAt: time.Now(),
@@ -61,37 +64,55 @@ func setupGlobal() {
 	alunEmail = utils.GetDummyEmail()
 
 	// Setup router
-	apiTester = test.NewAPITester(UserAPI)
+	apiTester = testutils.NewAPITester(UserAPI)
 }
 
 func tearDownGlobal() {
 }
 
 // Returns userId / User JWT token
-func setupUsers(t *testing.T, user User) (string, string) {
-	createdUser, err := createUser(user)
+func setupUsers(t *testing.T, user User, password string) (User, string) {
+	// Prefix email to avoid conflict between parallel tests
+	localUser := user
+	localUser.Email = fmt.Sprintf("%s%s", t.Name(), localUser.Email)
+
+	// Create user
+	createdUser, err := createUser(localUser)
 	if err != nil {
-		t.Errorf("Error when setup user %v: %v", user, err)
+		t.Errorf("Error when creating user %v: %v", user, err)
+	}
+
+	// Setup password
+	_, err = updatePassword(pwdChangeRequest{
+		Token:    localUser.PwdResetToken.Token,
+		Password: password,
+		Username: user.Username,
+	})
+	if err != nil {
+		t.Errorf("Error when setup password for user %v: %v", user, err)
 	}
 
 	jwt, _ := generateJWT(createdUser)
 
-	return createdUser.ID.Hex(), jwt.Jwt
+	return createdUser, jwt.Jwt
 }
 
 // Returns adminID, adminJwt, basicID, basicJwt
-func setupUserBasicAndAdmin(t *testing.T) (string, string, string, string) {
-	adminID, adminJwt := setupUsers(t, userAdmin)
-	basicID, basicJwt := setupUsers(t, userBasic)
+func setupUserBasicAndAdmin(t *testing.T) (User, string, User, string) {
+	admin, adminJwt := setupUsers(t, userAdmin, userAdminPassword)
+	basic, basicJwt := setupUsers(t, userBasic, userBasicPassword)
 
-	return adminID, adminJwt, basicID, basicJwt
+	return admin, adminJwt, basic, basicJwt
 }
 
 func tearDownBasicAndAdmin(t *testing.T) {
 	// db.al_users.deleteMany({ email: {"$in": ["alun.sng+1@gmail.com", "alun.sng+2@gmail.com"]} })
 	// { "acknowledged" : true, "deletedCount" : 2 }
 
-	toBeDeletedEmails := []string{userAdminEmail, userBasicEmail}
+	toBeDeletedEmails := []string{
+		fmt.Sprintf("%s%s", t.Name(), userAdminEmail),
+		fmt.Sprintf("%s%s", t.Name(), userBasicEmail),
+	}
 	filter := bson.M{
 		"email": bson.M{"$in": toBeDeletedEmails},
 	}
@@ -101,4 +122,12 @@ func tearDownBasicAndAdmin(t *testing.T) {
 	}
 
 	fmt.Printf("Deleted %d users with emails %+v\n", d.DeletedCount, toBeDeletedEmails)
+}
+
+func tearDownLogins(t *testing.T, userID primitive.ObjectID) {
+	filter := bson.M{"userId": userID}
+	_, err := dbUserLoginCollection.DeleteMany(context.TODO(), filter)
+	if err != nil {
+		t.Errorf("Error when cleaning login for userID %s\n", userID)
+	}
 }
