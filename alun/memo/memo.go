b29592e9cf8ab60d571678f8cc83ab202cb1feda
Al-un <alun.sng@gmail.com>
2020-05-17 16:58:21 +0900

    feat: defer memo initialisation in memo.go

--- alun/memo/memo.go
+++ alun/memo/memo.go
@@ -2,7 +2,27 @@
 package memo
 
 import (
+	"github.com/Al-un/alun-api/alun/utils"
 	"github.com/Al-un/alun-api/pkg/logger"
 )
 
-var memoLogger = logger.NewConsoleLogger(logger.LogLevelVerbose)
+var (
+	memoLogger logger.Logger
+)
+
+func init() {
+	if utils.IsTest() {
+		memoLogger = logger.NewSilenceLogger()
+	}
+
+	// --- Init logger
+	if memoLogger == nil {
+		memoLogger = logger.NewConsoleLogger(logger.LogLevelVerbose)
+	}
+
+	// --- Init DAO
+	initDao()
+
+	// ---- Init API
+	initAPI()
+}
