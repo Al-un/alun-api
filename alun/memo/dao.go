8d8d63594bdd2e6cf722fc107f4596f651b27025
Al-un <alun.sng@gmail.com>
2020-05-18 23:34:07 +0900

    feat: update dao for the new data model using  /

--- alun/memo/dao.go
+++ alun/memo/dao.go
@@ -2,6 +2,8 @@ package memo
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
 	"github.com/Al-un/alun-api/alun/core"
 	"github.com/Al-un/alun-api/alun/utils"
@@ -13,11 +15,9 @@ import (
 
 // ---------- Variable and init -----------------------------------------------
 var (
-	dbMemoCollectionName  string
-	dbMemoCollection      *mongo.Collection
-	dbBoardCollectionName string
-	dbBoardCollection     *mongo.Collection
-	returnOpt             options.ReturnDocument = 1
+	dbMemoCollectionName string
+	dbMemoCollection     *mongo.Collection
+	returnOpt            options.ReturnDocument = 1
 )
 
 // Init the connection with MongoDB upon app initialisation
@@ -28,11 +28,9 @@ func initDao() {
 	}
 
 	// Initialisation: collections name
-	dbBoardCollectionName = "al_memos_boards"
-	dbMemoCollectionName = "al_memos_memos"
+	dbMemoCollectionName = "al_memos"
 
 	// Initialisation: collections instances
-	dbBoardCollection = memoMongoDb.Collection(dbBoardCollectionName)
 	dbMemoCollection = memoMongoDb.Collection(dbMemoCollectionName)
 
 	memoLogger.Debug("[MongoDB] Memo initialisation!")
@@ -44,10 +42,15 @@ func findBoardsByUserID(userID string) ([]Board, *core.ServiceMessage) {
 	filter := bson.M{
 		core.TrackedCreatedBy: id,
 	}
+	options := &options.FindOptions{
+		Projection: bson.M{
+			"memos": 0,
+		},
+	}
 
-	var boards []Board
+	boards := make([]Board, 0)
 
-	cur, err := dbBoardCollection.Find(context.TODO(), filter)
+	cur, err := dbMemoCollection.Find(context.TODO(), filter, options)
 	if err != nil {
 		return boards, core.NewServiceErrorMessage(err)
 	}
@@ -61,66 +64,147 @@ func findBoardsByUserID(userID string) ([]Board, *core.ServiceMessage) {
 	return boards, nil
 }
 
-func findMemoByID(memoID string) (*Memo, *core.ServiceMessage) {
-	id, _ := primitive.ObjectIDFromHex(memoID)
-	filter := bson.M{"_id": id}
-
-	var memo Memo
-
-	if err := dbMemoCollection.FindOne(context.TODO(), filter).Decode(&memo); err != nil {
-		memoLogger.Debug("Memo fetching ID<%s/%s> error: ", memoID, id, err)
-		return nil, core.NewServiceErrorMessage(err)
-	}
-
-	return &memo, nil
-}
-
 func findBoardByID(boardID string) (*Board, *core.ServiceMessage) {
 	id, _ := primitive.ObjectIDFromHex(boardID)
 	filter := bson.M{"_id": id}
 
 	var board Board
 
-	if err := dbBoardCollection.FindOne(context.TODO(), filter).Decode(&board); err != nil {
+	if err := dbMemoCollection.FindOne(context.TODO(), filter).Decode(&board); err != nil {
 		return nil, core.NewServiceErrorMessage(err)
 	}
 
 	return &board, nil
 }
+func findMemoByID(boardID string, memoID string) (*Memo, *core.ServiceMessage) {
+	bID, _ := primitive.ObjectIDFromHex(boardID)
+	mID, _ := primitive.ObjectIDFromHex(memoID)
+	filter := bson.M{
+		"_id":       bID,
+		"memos._id": mID,
+	}
+	options := &options.FindOneOptions{
+		Projection: bson.M{
+			"memos.$": 1,
+		},
+	}
+
+	var board Board
+
+	if err := dbMemoCollection.FindOne(context.TODO(), filter, options).Decode(&board); err != nil {
+		return nil, core.NewServiceErrorMessage(err)
+	}
+
+	var memo Memo
+	memo = board.Memos[0]
+
+	return &memo, nil
+}
+
+// func findBoardWithMemosByID(boardID string) (*BoardWithMemo, *core.ServiceMessage) {
+// 	id, _ := primitive.ObjectIDFromHex(boardID)
+
+// 	matchStage := bson.D{primitive.E{Key: "$match", Value: bson.D{
+// 		primitive.E{Key: "_id", Value: id},
+// 	}}}
+// 	lookupStage := bson.D{primitive.E{Key: "$lookup", Value: bson.D{
+// 		primitive.E{Key: "from", Value: dbMemoCollectionName},
+// 		primitive.E{Key: "localField", Value: "_id"},
+// 		primitive.E{Key: "foreignField", Value: "boardId"},
+// 		primitive.E{Key: "as", Value: "memos"},
+// 	}}}
+// 	// matchStage := bson.D{primitive.E{Key: "$match", Value: bson.D{
+// 	// 	primitive.E{Key: "boardId", Value: id},
+// 	// }}}
+// 	// lookupStage := bson.D{primitive.E{Key: "$lookup", Value: bson.D{
+// 	// 	primitive.E{Key: "from", Value: dbBoardCollectionName},
+// 	// 	primitive.E{Key: "localField", Value: "boardId"},
+// 	// 	primitive.E{Key: "foreignField", Value: "_id"},
+// 	// 	primitive.E{Key: "as", Value: "memos"},
+// 	// }}}
+// 	// unwindStage := bson.D{primitive.E{Key: "$unwind", Value: bson.D{
+// 	// 	primitive.E{Key: "path", Value: "$memos"},
+// 	// 	primitive.E{Key: "preserveNullAndEmptyArrays", Value: false},
+// 	// }}}
+
+// 	cursor, err := dbBoardCollection.Aggregate(context.TODO(), mongo.Pipeline{matchStage, lookupStage})
+// 	if err != nil {
+// 		return nil, core.NewServiceErrorMessage(err)
+// 	}
+
+// 	// var memos []bson.M
+// 	var memos []BoardWithMemo
+// 	if err = cursor.All(context.TODO(), &memos); err != nil {
+// 		return nil, core.NewServiceErrorMessage(err)
+// 	}
+
+// 	fmt.Printf("Loaded: %+v\n", memos)
+
+// 	var board BoardWithMemo
+
+// 	return &board, nil
+// }
+
+func isBoardIDExist(boardID string) (bool, *core.ServiceMessage) {
+	id, _ := primitive.ObjectIDFromHex(boardID)
+	filter := bson.M{"_id": id}
+
+	count, err := dbMemoCollection.CountDocuments(context.TODO(), filter)
+	if err != nil {
+		return false, core.NewServiceErrorMessage(err)
+	}
+
+	return count > 0, nil
+}
+
+// func isMemoIDExist(memoID string) (bool, *core.ServiceMessage) {
+// 	id, _ := primitive.ObjectIDFromHex(memoID)
+// 	filter := bson.M{"_id": id}
+
+// 	count, err := dbMemoCollection.CountDocuments(context.TODO(), filter)
+// 	if err != nil {
+// 		return false, core.NewServiceErrorMessage(err)
+// 	}
+
+// 	return count > 0, nil
+// }
 
 func createBoard(toCreateBoard Board) (*Board, *core.ServiceMessage) {
-	insertResult, err := dbBoardCollection.InsertOne(context.TODO(), toCreateBoard)
+	toCreateBoard.ID = primitive.NewObjectID()
+
+	insertResult, err := dbMemoCollection.InsertOne(context.TODO(), toCreateBoard)
 	if err != nil {
 		return nil, core.NewServiceErrorMessage(err)
 	}
 
 	var newBoard Board
 	filter := bson.M{"_id": insertResult.InsertedID}
-	if err := dbBoardCollection.FindOne(context.TODO(), filter).Decode(&newBoard); err != nil {
+	if err := dbMemoCollection.FindOne(context.TODO(), filter).Decode(&newBoard); err != nil {
 		return nil, core.NewServiceErrorMessage(err)
 	}
 
 	return &newBoard, nil
 }
 
-func createMemo(toCreateMemo Memo) (*Memo, *core.ServiceMessage) {
+func createMemo(boardID string, toCreateMemo Memo) (*Memo, *core.ServiceMessage) {
 	memoLogger.Verbose("Creating %s with items %v", toCreateMemo.Title, toCreateMemo.Items)
+
 	toCreateMemo.ID = primitive.NewObjectID()
 
-	insertResult, err := dbMemoCollection.InsertOne(context.TODO(), toCreateMemo)
-	if err != nil {
-		memoLogger.Debug("Memo creation error: ", err)
+	bID, _ := primitive.ObjectIDFromHex(boardID)
+	filter := bson.M{
+		"_id": bID,
+	}
+	update := bson.M{
+		"$push": bson.M{
+			"memos": toCreateMemo,
+		},
+	}
+	if err := dbMemoCollection.FindOneAndUpdate(context.TODO(), filter, update).Err(); err != nil {
 		return nil, core.NewServiceErrorMessage(err)
 	}
 
-	var newMemo Memo
-	filter := bson.M{"_id": insertResult.InsertedID}
-	if err := dbMemoCollection.FindOne(context.TODO(), filter).Decode(&newMemo); err != nil {
-		memoLogger.Debug("Memo fetching error: ", err)
-		return nil, core.NewServiceErrorMessage(err)
-	}
-
-	return &newMemo, nil
+	return &toCreateMemo, nil
 }
 
 func updateBoard(boardID string, toUpdateBoard Board) (*Board, *core.ServiceMessage) {
@@ -132,43 +216,53 @@ func updateBoard(boardID string, toUpdateBoard Board) (*Board, *core.ServiceMess
 		ReturnDocument: &returnOpt,
 	}
 	update := bson.M{
-		"title":       toUpdateBoard.Title,
-		"description": toUpdateBoard.Description,
-		"access":      toUpdateBoard.Access,
-		"modifiedBy":  toUpdateBoard.ModifiedBy,
-		"modifiedAt":  toUpdateBoard.ModifiedAt,
+		"$set": bson.M{
+			"title":       toUpdateBoard.Title,
+			"description": toUpdateBoard.Description,
+			"access":      toUpdateBoard.Access,
+			"modifiedBy":  toUpdateBoard.ModifiedBy,
+			"modifiedAt":  toUpdateBoard.ModifiedAt,
+		},
 	}
 
 	var updatedBoard Board
-	if err := dbBoardCollection.FindOneAndUpdate(context.TODO(), filter, update, options).Decode(&updatedBoard); err != nil {
+	if err := dbMemoCollection.FindOneAndUpdate(context.TODO(), filter, update, options).Decode(&updatedBoard); err != nil {
 		return nil, core.NewServiceErrorMessage(err)
 	}
 
 	return &updatedBoard, nil
 }
 
-func updateMemo(memoID string, toUpdateMemo Memo) (*Memo, *core.ServiceMessage) {
-	id, _ := primitive.ObjectIDFromHex(memoID)
+func updateMemo(boardID string, memoID string, toUpdateMemo Memo) (*Memo, *core.ServiceMessage) {
+	bID, _ := primitive.ObjectIDFromHex(boardID)
+	mID, _ := primitive.ObjectIDFromHex(memoID)
 	filter := bson.M{
-		"_id": id,
+		"_id":       bID,
+		"memos._id": mID,
 	}
 	options := &options.FindOneAndUpdateOptions{
 		ReturnDocument: &returnOpt,
 	}
 	update := bson.M{
-		"title":       toUpdateMemo.Title,
-		"description": toUpdateMemo.Description,
-		"items":       toUpdateMemo.Items,
-		"modifiedBy":  toUpdateMemo.ModifiedBy,
-		"modifiedAt":  toUpdateMemo.ModifiedAt,
+		"$set": bson.M{
+			"memos.$.title":       toUpdateMemo.Title,
+			"memos.$.description": toUpdateMemo.Description,
+			"memos.$.items":       toUpdateMemo.Items,
+			"memos.$.modifiedBy":  toUpdateMemo.ModifiedBy,
+			"memos.$.modifiedAt":  toUpdateMemo.ModifiedAt,
+		},
 	}
 
-	var updatedMemo Memo
-	if err := dbMemoCollection.FindOneAndUpdate(context.TODO(), filter, update, options).Decode(&updatedMemo); err != nil {
+	if err := dbMemoCollection.FindOneAndUpdate(context.TODO(), filter, update, options).Err(); err != nil {
 		return nil, core.NewServiceErrorMessage(err)
 	}
 
-	return &updatedMemo, nil
+	updatedMemo, err := findMemoByID(boardID, memoID)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedMemo, nil
 }
 
 func deleteBoard(boardID string) (int64, int64, *core.ServiceMessage) {
@@ -176,7 +270,7 @@ func deleteBoard(boardID string) (int64, int64, *core.ServiceMessage) {
 
 	// Delete board
 	filter := bson.M{"_id": id}
-	deletedBoard, err := dbBoardCollection.DeleteMany(context.TODO(), filter, nil)
+	deletedBoard, err := dbMemoCollection.DeleteMany(context.TODO(), filter, nil)
 	if err != nil {
 		return -1, -1, core.NewServiceErrorMessage(err)
 	}
@@ -191,13 +285,31 @@ func deleteBoard(boardID string) (int64, int64, *core.ServiceMessage) {
 	return deletedBoard.DeletedCount, deletedMemos.DeletedCount, nil
 }
 
-func deleteMemo(memoID string) (int64, *core.ServiceMessage) {
-	id, _ := primitive.ObjectIDFromHex(memoID)
-	filter := bson.M{"_id": id}
-	d, err := dbMemoCollection.DeleteMany(context.TODO(), filter, nil)
+func deleteMemo(boardID string, memoID string) (int64, *core.ServiceMessage) {
+	_, err := findMemoByID(boardID, memoID)
 	if err != nil {
+		if err.HTTPStatus == http.StatusNotFound {
+			return -1, nil
+		}
+
+		return -1, err
+	}
+
+	bID, _ := primitive.ObjectIDFromHex(boardID)
+	mID, _ := primitive.ObjectIDFromHex(memoID)
+	filter := bson.M{
+		"_id": bID,
+	}
+	update := bson.M{
+		"$pull": bson.M{
+			"memos": bson.M{"_id": mID},
+		},
+	}
+
+	if err := dbMemoCollection.FindOneAndUpdate(context.TODO(), filter, update).Err(); err != nil {
+		fmt.Println(err)
 		return -1, core.NewServiceErrorMessage(err)
 	}
 
-	return d.DeletedCount, nil
+	return 1, nil
 }
